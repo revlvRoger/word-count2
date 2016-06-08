cc9b8df1a16bef1e378519e80ed98df7d837ca51
revlvRoger <revlv.roger@gmail.com>
2016-06-08 14:10:43 +0800

    functions added

--- csv.php
+++ csv.php
@@ -1,13 +1,41 @@
-<?php
-$csvFile = file_get_contents('todo.csv');
-var_dump($csvFile);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<table border="2px">
+    <h1>CSV File</h1>
 
-// while (($line = fgetcsv($fileCsv)) !== false) {
-//                     echo "<tr>";
-//                     foreach ($line as $cell) {
-//                         echo "<td>" . htmlspecialchars($cell) . "</td>";
-//                     }
-//                     echo "</tr>\n";
-//             }
-//             fclose($f);
-//             ?>
+
+<?php
+$fileCsv = fopen("todo.csv", "r");
+
+class FileCsv
+{
+    public function uploadCsv($fileCsv)
+    {
+        echo "<tr>
+                <td>#</td>
+                <td>Game</td>
+                <td>Description</td>
+            </tr>";
+
+        while (($line = fgetcsv($fileCsv)) !== false)
+         {
+            echo "<tr>";
+            foreach ($line as $cell) {
+                echo "<td>" . htmlspecialchars($cell) . "</td>";
+            }
+            echo "</tr>\n";
+        }
+    }
+}
+$csv = new FileCsv();
+$csv->uploadCsv($fileCsv);
+
+?>
+</table>
+</body>
+</html>
--- upload.php
+++ upload.php
@@ -7,40 +7,25 @@
 <body>
 <h1>Upload Image</h1>
 <?php
-if(isset($_POST['Submit'])){
-    // loop through the uploaded files
-    foreach ($_FILES as $key => $value){
-        $image_tmp = $value['tmp_name'];
-        $image = $value['name'];
-        $image_file = "{$UPLOADDIR}{$image}";
 
-        // move the file to the permanent location
-        if(move_uploaded_file($image_tmp,$image_file)){
-            echo <<<HEREDOC
-    <img src="{$image_file}" alt="file not found" /></br>
-</div>
-HEREDOC;
-        }
-        else{
-            echo "<h1>image file upload failed, image too big after compression</h1>";
-        }
+$image = __DIR__. '/'. md5(date('Y-m-d H:i:s')) . '.jpg';
+
+class Upload
+{
+    public function uploadedFile($image)
+    {
+        move_uploaded_file($_FILES['image']['tmp_name'], $image);
     }
 }
-else{
-?>
- <?php
-        // var_dump($_FILES['image'], __DIR__);
-        $filename = __DIR__. '/'. md5(date('Y-m-d H:i:s')) . '.jpg';
+    $uploaded = new Upload();
+    $uploaded->uploadedFile($image);
 
-        move_uploaded_file($_FILES['image']['tmp_name'], $filename);
-    ?>
+
+?>
     <form method='POST' enctype='multipart/form-data'>
         <input type='file' name='image'><br><br>
         <input name='Submit' type='submit' value='Upload image'>
         <button type="reset" value="Reset">Reset</button>
     </form>
-<?php
-}
-?>
 </body>
 </html>
